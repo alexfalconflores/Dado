2407f2781c39dd2f99668fa52498775a194adceb
Alex Falcon <alexfalconflores@hotmail.com>
2020-12-05 14:31:13 -0500

    Dado terminado con los valores number convertidos en string para evitar errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dado';
+
+  valor1 = '';
+  valor2 = '';
+  valor3 = '';
+  resultado = '';
+
+  constructor()
+  {
+    this.valor1 = this.retornarAleatorio().toString();
+    this.valor2 = this.retornarAleatorio().toString();
+    this.valor3 = this.retornarAleatorio().toString();
+  }
+
+  // tslint:disable-next-line: typedef
+  retornarAleatorio()
+  {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  // tslint:disable-next-line: typedef
+  tirar()
+  {
+    this.valor1 = this.retornarAleatorio().toString();
+    this.valor2 = this.retornarAleatorio().toString();
+    this.valor3 = this.retornarAleatorio().toString();
+
+    // tslint:disable-next-line: triple-equals
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3)
+    {
+      this.resultado = 'Ganó';
+    }
+    else
+    {
+      this.resultado = 'Perdió';
+    }
+  }
+
 }
